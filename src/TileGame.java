bb28c36229e386121cb79790b4d8a835789d1a2c
BTHS Java A Class - Mr. F <nfolwell@schools.nyc.gov>
2023-03-06 12:22:18 -0500

    took out answers!

--- src/TileGame.java
+++ src/TileGame.java
@@ -16,37 +16,12 @@ public class TileGame {
 
     private int getIndexForFit(NumberTile tile)
     {
-        if ((this.board.size() == 0) ||
-                (tile.getRight() == this.board.get(0).getLeft()))
-            return 0;
-        for (int i = 1; i < this.board.size(); i++)
-        {
-            if (tile.getLeft() == this.board.get(i-1).getRight() &&
-                    tile.getRight() == this.board.get(i).getLeft())
-                return i;
-        }
-        if (tile.getLeft() == this.board.get(this.board.size() - 1).getRight())
-            return this.board.size();
-        return -1;
+       return -1;
     }
 
     public boolean insertTile(NumberTile tile)
     {
-        int index = getIndexForFit(tile);
-        System.out.println("index = "+index);
-        int test = 1;
-        System.out.println(tile);
-        while (index == -1 && test < 4)
-        {
-            tile.rotate();
-            System.out.println(tile);
-            index = getIndexForFit(tile);
-            System.out.println("index = "+index);
-            test++;
-        }
-        if (index != -1)
-            this.board.add(index, tile);
-        return (index != -1);
+        return false;
     }
     public String toString()
     {
